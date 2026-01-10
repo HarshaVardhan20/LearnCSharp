781529d7c1c11aeffd26b364cd8ad9c306788cc8
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2026-01-10 12:19:34 +0530

    Custom exception

---
+++ AppCustomeException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+    public class AppCustomException : Exception
+    {
+        public override string Message => "Internal Exception";
+    }
+}
