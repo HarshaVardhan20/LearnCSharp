b2930f87c0413974865831417ee4651602136ad4
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2026-01-14 11:47:01 +0530

    GenericsTypes

---
+++ ExampleWorkoutGernericsLinq.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+    public class EmployeeRe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+    public class ExampleWorkoutGernericsLinq
+    {
+        public static List<EmployeeRe> EmployeeReList = new List<EmployeeRe>();
+
+        public ExampleWorkoutGernericsLinq()
+        {
+            
+        }
+        public static void Main(string[] args)
+        {
+            var localEmpList = GetData();
+            var filterList = from emp in localEmpList
+                             select emp;
+
+            foreach (EmployeeRe emp in filterList)
+            {
+                 
+            }
+        }
+
+        public static List<EmployeeRe> GetData()
+        {
+            EmployeeReList.Add(new EmployeeRe() { Id=1, Name ="A", Rank=1 });
+            EmployeeReList.Add(new EmployeeRe() { Id=2, Name ="B", Rank=3 });
+            EmployeeReList.Add(new EmployeeRe() { Id=3, Name ="C", Rank=2 });
+
+            return EmployeeReList;
+        }
+    }
+}
