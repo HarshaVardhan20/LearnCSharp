402e221bda05b271f2689ab143bb2b234638f1ff
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2026-01-14 12:22:12 +0530

    Functions Action Predicate

--- CreateGenerics.cs
+++ CreateGenerics.cs
@@ -30,13 +30,81 @@ namespace LearningCSharp
     {
         public static void Main(string[] args)
         {
-            MyGlobalType<UGStudent> myGlobalType = new MyGlobalType<UGStudent>();
-            //MyGlobalType<Object> myGlobalType1 = new MyGlobalType<Object>();
+            Action<string> logger = NewMethod();
 
-            UGStudent obj = new UGStudent();
-            string result = myGlobalType.GetDataType(obj);
-            Console.WriteLine(result);
-            Console.ReadLine();
+            if (DateTime.Now.Hour < 12)
+            {
+                logger = GoodMoring();
+            }
+            else
+            {
+                logger = GoodDay();
+            }
+
+            logger("dd");
+
+
+
+
+            logger = Method2();
+
+            logger("Application Started"); // Invoking the Action
+
+        }
+
+        private static Action<string> GoodDay()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Action<string> NewMethod()
+        {
+            //MyGlobalType<UGStudent> myGlobalType = new MyGlobalType<UGStudent>();
+            ////MyGlobalType<Object> myGlobalType1 = new MyGlobalType<Object>();
+
+            //UGStudent obj = new UGStudent();
+            //string result = myGlobalType.GetDataType(obj);
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+
+
+            return message =>
+            {
+                Console.WriteLine($"[LOG]: {message} at {DateTime.Now}");
+            };
+        }
+
+        private static Action<string> GoodDay(string str)
+        {
+            return message =>
+            {
+                Console.WriteLine($"Good Day to you");
+            };
+        }
+
+        private static Action<string> GoodMoring()
+        {
+            return message =>
+            {
+                Console.WriteLine($"Good Morning");
+            };
+        }
+
+        private static Action<string> Method1()
+        {
+            return message =>
+            {
+                Console.WriteLine($"[LOG]: {message.ToUpper()} at {DateTime.Now}");
+            };
+        }
+
+        private static Action<string> Method2()
+        {
+            return message =>
+            {
+                Console.WriteLine($"Welcome to Programming");
+            };
         }
     }
     public class MyGlobalType<T> where T : Student
